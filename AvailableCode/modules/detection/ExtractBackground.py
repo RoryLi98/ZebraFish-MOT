95a005f2dad50dbea892931b7b9c19d97e0c4ba4
Link <sherlock-link@hotmail.com>
2021-05-12 23:49:49 +0800

    update 5.12

---
+++ AvailableCode/modules/detection/ExtractBackground.py
@@ -0,0 +1,178 @@
+import cv2
+import argparse
+import os.path
+import sys
+import configparser
+import numpy as np
+sys.path.append('D:\\bishe\\aauvap-3d-zef-7230261dcb99\\aauvap-3d-zef-7230261dcb99\\')
+from common.utility import *
+
+class BackgroundExtractor():
+    """
+    Class implementation for background subtraction
+    """
+    
+    def __init__(self, dataPath, camId, video=False):
+        """
+        Initialize object
+        
+        Input:
+            vidPath: Path to the video files
+        """
+        
+        print("BackgroundExtractor initialized.")
+        
+        self.bgPath = os.path.join(dataPath, 'background_cam{0}.png'.format(camId))
+        self.video = video
+
+        if self.video:
+            self.vidPath = os.path.join(dataPath, 'cam{0}.mp4'.format(camId))
+
+            # Load video
+            cap = cv2.VideoCapture(self.vidPath)
+        
+            # Close program if video file could not be opened
+            if not cap.isOpened():
+                print("Could not open file: {0}".format(self.vidPath))
+                sys.exit()
+            self.cap = cap
+        else:
+            self.imgPath = os.path.join(dataPath, 'cam{0}'.format(camId))
+        
+            if not os.path.isdir(self.imgPath):
+                print("Could not find image folder {0}".format(self.imgPath))
+                sys.exit()
+
+
+        if(os.path.isfile(self.bgPath)):
+            print("Background file already exists. Exiting...")
+            return
+        # self.n_median = 80
+        self.loadSettings(dataPath)
+
+
+    def loadSettings(self,path):
+        """
+        Load settings from config file in the provided path.
+        
+        Config file includes information on the following, which is set in the object:
+            n_median: Number of images to use for calculating the median image
+            
+        Input:
+            path: String path to the folder where the settings.ini file is located
+        """
+        
+        config = readConfig(path)
+        c = config['BackgroundExtractor']
+        self.n_median = c.getint("n_median")
+
+
+    def _collectSamplesVideo(self, verbose = False):
+        """
+        Sample frames for the background calculation by extracting them from the defined video.
+
+        Input:
+            verbose: Whether to print information regarding which frames are used
+        """
+
+        cap = self.cap
+        # Collect sample images from the video
+        imgs = []
+        numSamples = self.n_median
+        maxFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        sampleFrames = np.linspace(0,maxFrames,numSamples+2)[1:-1]
+
+
+        for f in sampleFrames:
+            if verbose:
+                print("Extracting image from frame: {0}".format(int(f)))
+            cap.set(cv2.CAP_PROP_POS_FRAMES,int(f))
+            ret, frame = cap.read()
+            if(ret):
+                imgs.append(frame)
+        self.imgs = imgs
+        
+        
+    def _collectSamplesImg(self, verbose = False):
+        """
+        Sample frames for the background calculation by extracting them from the prior extracted image files.
+
+        Input:
+            verbose: Whether to print information regarding which frames are used
+        """
+
+        imgs = []
+        frames = [f for f in sorted(os.listdir(self.imgPath)) if os.path.splitext(f)[-1] in [".png", ".jpg"]]
+        numSamples = self.n_median
+        maxFrames = len(frames)
+        sampleFrames = np.linspace(0, maxFrames, numSamples, endpoint = False, dtype=np.int)
+
+        for f in sampleFrames:
+            if verbose:
+                print("Extracting image from frame: {0}".format(int(f)))
+            frame = cv2.imread(os.path.join(self.imgPath, frames[f]))
+            if frame is not None:
+                imgs.append(frame)
+        self.imgs = imgs
+
+
+
+
+    def collectSamples(self, verbose = False): 
+        """
+        Collect samples to be used when calculating background image.
+        The images are sampled at uniform intervals throughout the provided video or img folder
+        """
+        
+        if self.video:
+            self._collectSamplesVideo()
+        else:
+            self._collectSamplesImg()
+    
+
+    def createBackground(self):
+        """
+        Compute the median image of the collected samples.
+        
+        Output:
+            bg: The computed background image
+        """
+        
+        try:
+            # Merge samples into background and save
+            print("Generating background from {0} samples".format(len(self.imgs)))
+            stack = np.stack(self.imgs)
+            bg = np.median(stack, axis=0)
+            return bg
+        except:
+            print("No images given. Run BackgroundExtractor.collectSamples before trying to create a background")
+            sys.exit() 
+
+
+if __name__ == '__main__':
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--path", help="Path to the directory containing the video file")
+    ap.add_argument("-c", "--camera", help="The ID of the camera to process. Top = 1. Front = 2.")
+    
+    args = vars(ap.parse_args())
+
+    if args.get("path", None) is None:
+        print('No path was provided. Try again!')
+        sys.exit()
+    else:
+        path = args["path"]
+    
+    if args.get("camera", None) is None:
+        camId = 1
+        print('No camera ID was given. Using default ID of: {0}'.format(camId))
+    else:
+        camId = int(args["camera"])
+     
+    # Prepare path
+    bgPath = os.path.join(path, 'background_cam{0}.png'.format(camId))
+
+    bgExt = BackgroundExtractor(path, camId)
+    bgExt.collectSamples()
+    bg = bgExt.createBackground()
+    cv2.imwrite(bgPath, bg)
---
+++ AvailableCode/modules/fasterrcnn/evaluateVideo.py
@@ -0,0 +1,127 @@
+import os 
+import utils
+import torch
+import argparse     
+import datetime
+from torchvision import models
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.transforms import functional as F
+import pickle as pkl
+import cv2
+from PIL import Image
+import pandas as pd                  
+from custom_dataset import CustomDataset                                                                                                                                                                      
+
+def initializeModel(pretrained, num_classes):
+    """
+    Loads the Faster RCNN ResNet50 model from torchvision, and sets whether it is COCO pretrained, and adjustes the heds box predictor to our number of classes.
+
+    Input:
+        pretrained: Whether to use a CoCo pretrained model
+        num_classes: How many classes we have:
+
+    Output:
+        model: THe initialized PyTorch model
+    """
+    
+    # Load model
+    model = models.detection.fasterrcnn_resnet50_fpn(pretrained=pretrained, pretrained_backbone=pretrained)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    return model
+    
+@torch.no_grad()
+def test(args):
+
+    path = args["path"]
+    camId = args["camId"]
+    weight_path = args["weights"]
+    startFrame = args["startFrame"]
+    endFrame = args["endFrame"]
+    outputPath = args["outputPath"]
+
+    if not os.path.isdir(outputPath):
+        os.makedirs(outputPath)
+
+    # train on the GPU or on the CPU, if a GPU is not available 
+    if torch.cuda.is_available():
+        device = torch.device('cuda') 
+        print("Using GPU")
+    else:
+        print("WARNING: Using CPU")
+        device = torch.device('cpu')
+
+    # our dataset has two classes only - background and zebrafish
+    num_classes = 2
+
+    model = initializeModel(False, num_classes)
+    model.load_state_dict(torch.load(weight_path)["model_state_dict"])
+
+    cpu_device = torch.device("cpu")
+
+    vid_path = os.path.join(path, "cam{}.mp4".format(camId))
+
+    cap = cv2.VideoCapture(vid_path)
+
+    if not cap.isOpened():
+        print("VIDEO NOT FOUND: {}".format(vid_path))
+
+    # move model to the device (GPU/CPU)
+    model.to(device)
+    model.eval()
+
+    id_map = {0:"Background", 1:"Zebrafish"}
+    output_df = pd.DataFrame(columns=["Filename", "Object ID", "Annotation tag", "Upper left corner X", "Upper left corner Y", "Lower right corner X", "Lower right corner Y", "Confidence"])
+
+    frameCount = 0
+    while(cap.isOpened()):
+        ret, img = cap.read()
+        frameCount += 1
+
+        if frameCount % 1000 == 0:
+            print(frameCount)
+
+        if frameCount < startFrame:
+            continue
+
+        if frameCount > endFrame:
+            frameCount -= 1
+            break
+
+        if ret:
+            img = F.to_tensor(img)
+            img = [img.to(device)]
+
+            outputs = model(img)
+            outputs = {k: v.to(cpu_device) for k, v in outputs[0].items()}
+
+            bboxes = outputs["boxes"].cpu().detach().numpy()
+            labels = outputs["labels"].cpu().detach().numpy()
+            scores = outputs["scores"].cpu().detach().numpy()
+            filename = str(frameCount).zfill(6)+".png"
+
+            output_dict = {"Filename": [filename]*len(scores),
+                            "Frame": [frameCount]*len(scores),
+                            "Object ID": [-1]*len(scores),
+                            "Annotation tag": [id_map[x] for x in labels],
+                            "Upper left corner X": list(bboxes[:,0]),
+                            "Upper left corner Y": list(bboxes[:,1]),
+                            "Lower right corner X": list(bboxes[:,2]),
+                            "Lower right corner Y": list(bboxes[:,3]),
+                            "Confidence": list(scores)}
+            output_df = pd.concat([output_df, pd.DataFrame.from_dict(output_dict)], ignore_index=True)
+        else:
+            break
+    output_df.to_csv(os.path.join(outputPath, "boundingboxes_2d_cam{}.csv".format(camId)), index=False, sep=",")
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--path", help="Path to folder")
+    ap.add_argument("-c", "--camId", help="Camera ID. top = 1 and front = 2")
+    ap.add_argument("-w", "--weights", help="Path to the trained model weights")
+    ap.add_argument("-sf", "--startFrame", default=1, type=int)
+    ap.add_argument("-ef", "--endFrame", default=10000, type=int)
+    ap.add_argument("-o", "--outputPath", default="./processed")
+    args = vars(ap.parse_args())
+    test(args)
